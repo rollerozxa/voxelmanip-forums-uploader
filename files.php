b28787b4c13927a1b3d6b6716a95389af101c202
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-06-30 23:39:28 +0200

    Add pagination to files page

--- files.php
+++ files.php
@@ -2,11 +2,15 @@
 require('lib/common.php');
 
 $id = $_GET['id'] ?? 0;
+$page = $_GET['page'] ?? 1;
+
+$fpp = 50;
 
 $category = $sql->fetch("SELECT * FROM uploader_categories WHERE id = ?", [$id]);
 
 $ufields = userfields('u', 'u');
-$files = $sql->query("SELECT f.*, $ufields FROM uploader_files f JOIN users u ON u.id = f.user WHERE f.cat = ? ORDER BY date", [$id]);
+$files = $sql->query("SELECT f.*, $ufields FROM uploader_files f JOIN users u ON u.id = f.user WHERE f.cat = ? ORDER BY date DESC LIMIT ?,?",
+	[$id, ($page - 1) * $fpp, $fpp]);
 
 _pageheader($category['name']);
 
@@ -46,8 +50,11 @@ if ($log && $loguser['powerlevel'] > 0) {
 		<!--<td class="b h" width="100">Downloads</td>-->
 	</tr><?php
 
+$zebra = 1;
 while ($file = $files->fetch()) {
-	?><tr class="n2">
+	$zebra = ($zebra == 1 ? 2 : 1);
+
+	?><tr class="n<?=$zebra?>">
 		<td class="b"><a href="get.php?id=<?=$file['id']?>"><?=$file['filename']?></a></td>
 		<td class="b"><?=$file['description']?></td>
 		<td class="center b"><?=userlink($file, 'u')?></td>
@@ -57,7 +64,8 @@ while ($file = $files->fetch()) {
 	</tr><?php
 
 }
+if_empty_query($category['files']+1, "No files uploaded.", 5);
 
-echo '</table>';
+echo '</table>'.pagelist($category['files'], $fpp, "files.php?id=".$id, $page);
 
 _pagefooter();
